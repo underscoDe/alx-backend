7a4965fad6c558d89af207adec8d21ee87b4e20e
Arsene Awounou <developer.awounou@gmail.com>
2022-07-10 16:14:08 +0100

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -31,4 +31,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
+        """finds the correct indexes to paginate the dataset \
+            correctly and return the appropriate page of the dataset
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        return [] if (start >= len(dataset) or
+                      end >= len(dataset)) else dataset[start:end]
